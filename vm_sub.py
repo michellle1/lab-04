1caecd5938ce5d1f724aa1866f3072a4fea97854
Michelle Arredondo <mearredo@usc.edu>
2023-02-20 22:20:40 -0800

    Update vm_sub.py

--- vm_sub.py
+++ vm_sub.py
@@ -1,3 +1,6 @@
+#Lab Partners: Michelle Arredondo and Nayeli De Leon
+#https://github.com/michellle1/lab-04
+
 """EE 250L Lab 04 Starter Code
 Run vm_pub.py in a separate terminal on your VM."""
 
@@ -14,10 +17,13 @@ def on_connect(client, userdata, flags, rc):
 
     print("Connected to server (i.e., broker) with result code "+str(rc))
     #replace user with your USC username in all subscriptions
-    client.subscribe("user/ipinfo")
+    client.subscribe("mearredo/ipinfo")
+    client.subscribe("mearredo/date")
+    client.subscribe("mearredo/time")
     
     #Add the custom callbacks by indicating the topic and the name of the callback handle
-    client.message_callback_add("user/ipinfo", on_message_from_ipinfo)
+    client.message_callback_add("mearredo/ipinfo", on_message_from_ipinfo)
+    client.message_callback_add("mearredo/date", on_message_from_date)
 
 
 """This object (functions are objects!) serves as the default callback for 
@@ -30,6 +36,10 @@ def on_message(client, userdata, msg):
 #Custom message callback.
 def on_message_from_ipinfo(client, userdata, message):
    print("Custom callback  - IP Message: "+message.payload.decode())
+   
+   
+def on_message_from_date(client, userdata, message):
+   print("Custom callback  - Date: "+message.payload.decode())
 
 
 
